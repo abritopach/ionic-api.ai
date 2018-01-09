6fdecd25019332ae786e0692596bdc71907eca94
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-01-09 12:52:24 +0100

    Added api.ai

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Platform } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
+
+declare var ApiAIPromises: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  answer: any;
 
+  constructor(public navCtrl: NavController, public platform: Platform, public ngZone: NgZone) {
+    platform.ready().then(() => {
+      ApiAIPromises.init({
+        clientAccessToken: "52b5d54f14d94d649a5998918b396628"
+      })
+      .then((result) =>  console.log(result))
+    });
+  }
+
+  ask(question) {
+    ApiAIPromises.requestText({
+      query: question
+    })
+    .then(({result: {fulfillment: {speech}}}) => {
+       this.ngZone.run(()=> {
+         this.answer = speech;
+       });
+    })
   }
 
 }
