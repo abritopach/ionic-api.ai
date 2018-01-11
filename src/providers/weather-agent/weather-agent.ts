98419321d084daa2cb5455c69af80d0df5a3d53e
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2018-01-11 17:39:54 +0100

    Updated weather agent

--- src/providers/weather-agent/weather-agent.ts
+++ src/providers/weather-agent/weather-agent.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
-import { ApiAiClient } from 'api-ai-javascript';  
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';  
+import { Injectable } from "@angular/core";
+import { ApiAiClient } from "api-ai-javascript";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
 
 /*
   Generated class for the WeatherAgentProvider provider.
@@ -9,50 +9,49 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
   and Angular DI.
 */
 
-export class Message {  
-
-  constructor(public msg: string, public from: string) { }  
-
-}  
+export class Message {
+  constructor(public msg: string, public from: string, public iconURL) {}
+}
 
 @Injectable()
 export class WeatherAgentProvider {
-
-  private readonly client = new ApiAiClient({ accessToken: '52b5d54f14d94d649a5998918b396628' });  
+  private readonly client = new ApiAiClient({
+    accessToken: "52b5d54f14d94d649a5998918b396628"
+  });
   conversation = new BehaviorSubject<Message[]>([]);
 
   constructor() {
-    console.log('Hello WeatherAgentProvider Provider');
+    console.log("Hello WeatherAgentProvider Provider");
   }
 
   // Sends and receives messages via DialogFlow.
-  talk(msg: string) {  
-
-    const userMessage = new Message(msg, 'user');  
+  talk(msg: string) {
+    const userMessage = new Message(msg, "user", "");
     //console.log(userMessage);
 
-    this.update(userMessage);  
+    this.update(userMessage);
 
-    return this.client.textRequest(msg)  
+    return this.client
+      .textRequest(msg)
 
-      .then(res => {  
+      .then(res => {
+        const speech = res.result.fulfillment.speech;
+        let iconURL = "";
+        if (typeof res.result.fulfillment["data"] !== "undefined") {
+          iconURL = res.result.fulfillment["data"].iconURL;
+        }
 
-        const speech = res.result.fulfillment.speech; 
-        
-        //console.log("speech", speech);
+        const botMessage = new Message(speech, "bot", iconURL);
 
-        const botMessage = new Message(speech, 'bot');  
-
-        this.update(botMessage);  
-
+        this.update(botMessage);
       })
-      .catch((error) => {/* do something here too */});  
-
-  }  
+      .catch(error => {
+        /* do something here too */
+      });
+  }
 
   // Adds message to source.
-  update(msg: Message) {  
-    this.conversation.next([msg]);  
-  }  
-
+  update(msg: Message) {
+    this.conversation.next([msg]);
+  }
 }
